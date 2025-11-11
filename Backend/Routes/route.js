57889ef7c93665a3634c9c25272b3465814260cd
DanielAlexander912 <00232622@uca.edu.sv>
2025-11-10 21:20:49 -0600

    Adding FE and BE modules

---
+++ Backend/Routes/route.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import { verifyToken } from '../Middlewares/verifyToken.js';
+
+import { displayHome } from '../Controllers/displayHome.js';
+import { signIn } from '../Controllers/signIn.js';
+import { signUp } from '../Controllers/signUp.js';
+import { getUsers, getUserById } from '../Controllers/getUsers.js';
+import { createUser } from '../Controllers/createUser.js';
+import { updateUser } from '../Controllers/updateUser.js';
+import { deleteUser } from '../Controllers/deleteUser.js';
+
+const routes = express.Router();
+
+routes.get('/', displayHome);
+
+routes.get('/signIn', signIn);
+routes.get('/signUp', signUp);
+
+routes.get('/users', verifyToken, getUsers);
+routes.get('/users/:id', verifyToken, getUserById);
+routes.post('/users', verifyToken, createUser);
+routes.put('/users/:id', verifyToken, updateUser);
+routes.delete('/users/:id', verifyToken, deleteUser);
+
+export default routes;
